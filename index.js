be9ccab7492ba296973095c2f602a01d9fff60c1
VinitAsutkar <vinit123asutkar@gmail.com>
2022-10-02 19:58:30 +0530

    final submission

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("mint").addEventListener("mouseover",function(){
+    document.getElementById("media-statemnt").innerText="Warburg invests $100 mn in boAt";
+});
+document.getElementById("forbes").addEventListener("mouseover",function(){
+    document.getElementById("media-statemnt").innerText="How India's boAt became the fifth largest wearable brand in the world";
+});
+document.getElementById("techradar").addEventListener("mouseover",function(){
+    document.getElementById("media-statemnt").innerText="The Airdopes 441 is sweat proof and packs in crisp audio. Great for fitness enthusiasts.";
+});
+document.getElementById("et").addEventListener("mouseover",function(){
+    document.getElementById("media-statemnt").innerText="IPL 2020: boAt becomes the official audio partner for six teams";
+});
+document.getElementById("gadget360").addEventListener("mouseover",function(){
+    document.getElementById("media-statemnt").innerText="TWS Earbuds Shipments in India Up 723 Percent YoY in Q3, Boat Emerged as Leading Player: Counterpoint";
+});
